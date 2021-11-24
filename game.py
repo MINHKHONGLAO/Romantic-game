0dd3e7e362b3b915e44dcf48f3f4db5741bca55f
MINHKHONGLAO <94971874+MINHKHONGLAO@users.noreply.github.com>
2021-11-24 19:58:31 +0700

    Update game.py

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ haiY_change = []
 num_of_hai = 5
 
 for i in range(num_of_hai):
-    haiImg.append(pygame.image.load('hai.png'))
+    haiImg.append(pygame.image.load('minh.png'))
     haiX.append(random.randint(0,735))
     haiY.append(random.randint(20,150))
     haiX_change.append(0.2)
@@ -32,7 +32,7 @@ for i in range(num_of_hai):
         screen.blit(haiImg[i],(x,y))
 
 # your girl friend
-nganImg = pygame.image.load('ngan.png')
+nganImg = pygame.image.load('quynh.png')
 nganX = 380
 nganY = 500
 nganX_change = 0
